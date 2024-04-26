221c5b8b9c1f2298c68498f8f2203b631da36a86
Nazar <example"gmail.com>
2024-04-26 14:29:16 +0300

    Routes were added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import AdvertismentDetailsPage from "./pages/AdvertismentDetailsPage";
+import RequestsPage from "./pages/RequestsPage";
+import CreateRequestPage from "./pages/CreateRequestPage";
+import ErrorPage from "./pages/ErrorPage";
 import StartPage from "./pages/StartPage";
 
-const router = createBrowserRouter([{ path: "/", element: <StartPage /> }]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <StartPage />,
+    errorElement: <ErrorPage />,
+  },
+  { path: "/home", element: <HomePage /> },
+  { path: "/home/advId", element: <AdvertismentDetailsPage /> },
+  { path: "/requests", element: <RequestsPage /> },
+  { path: "/requests/create-request", element: <CreateRequestPage /> },
+]);
 function App() {
   return <RouterProvider router={router} />;
 }
